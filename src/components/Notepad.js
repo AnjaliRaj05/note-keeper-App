98917b8f2e795fcc4c71b3383f0af01bcb54f2f2
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-06-25 14:39:39 +0530

    initial

---
+++ src/components/Notepad.js
@@ -0,0 +1,35 @@
+// // import React from 'react'
+
+// // export default function Notepad() {
+
+// //   return (
+// //     <div>
+      
+// //     </div>
+// //   )
+// // }
+
+// const Notepad =[
+//  {
+//     key:1,
+//     title: 'Description',
+//     content:"lorem20",
+//  },
+
+//  {
+//     key:2,
+//     title: 'Notepadffvjg',
+//     content:"jgggrrlj4i4jrdfhhufheifjidrj",
+//  },
+//  {
+//     key:3,
+//     title: 'Notepadffvjg',
+//     content:"jgggrrlj4i4jrdfhhufheifjidrj",
+//  },
+//  {
+//     key:4,
+//     title: 'Notepadffvjg',
+//     content:"jgggrrlj4i4jrdfhhufheifjidrj",
+//  },
+// ];
+//  export default Notepad;
